cc039fb8703ae871eed9f3fe5d84f1b88a257ef9
arie0703 <[ari0703sophia@gmail.com]>
2022-07-12 07:56:07 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## ansibleローカル実行用
+- get_cache.sh
+    - ~/Library/Caches内のファイルサイズを出力
---
+++ scripts/get_cache.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+du -h -d 1 ~/Library/Caches | sort -h >> ~/downloads/projects/ansible-local/output/cache.txt
